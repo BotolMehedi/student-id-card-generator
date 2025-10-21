ded88ddb23ad00f84f753b65b44323758afe87e4
Md Miraz Islam <64999484+BotolMehedi@users.noreply.github.com>
2025-10-21 07:22:44 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Load clg data
 let colleges = {};
